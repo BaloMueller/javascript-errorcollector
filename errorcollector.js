3397db6502436479b137388ecba15ff20395068b
Sebastian Müller <balomueller@googlemail.com>
2013-04-09 19:46:29 +0200

    In this case AMD Modules are the wrong approach

--- errorcollector.js
+++ errorcollector.js
@@ -1,24 +1,19 @@
-'use strict';
+var otherOnerror = window.onerror;
+window.onerror = function(msg,url,l) {
+    'use strict';
 
-define('errorcollector', [], function() {
+    if (otherOnerror) {
+		otherOnerror(msg, url, l);
+	}
 
-	var otherOnerror = window.onerror;
-	window.onerror = function(msg,url,l) {
-		if(otherOnerror) {
-			otherOnerror(msg, url, l);
-		}
-
-		var bag = {
-			'msg': msg,
-			'url': url,
-			'line': line,
-			'stack': stack,
-			'platform': navigator.platform,
-			'userAgent': navigator.userAgent
-		};
-
-
-		$.post("/diagnostics/javascriptexceptions", bag):
+	var bag = {
+		'msg': msg ? msg : '',
+		'url': url ? url : '',
+		'line': l ? l : '',
+		'platform': navigator.platform,
+		'userAgent': navigator.userAgent
 	};
 
-});
+
+	$.post("/diagnostics/javascriptexceptions", bag);
+};
