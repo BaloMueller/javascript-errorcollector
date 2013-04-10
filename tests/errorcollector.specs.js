647474d10d32741e71b41ce5c7cb911ce6e35ac7
Sebastian Müller <balomueller@googlemail.com>
2013-04-10 13:00:10 +0200

    Fixed Test

--- tests/errorcollector.specs.js
+++ tests/errorcollector.specs.js
@@ -5,36 +5,24 @@
 		throw "unhandled exception";	
 	}, 0);
 
-xdescribe('Always', function() {
+describe('Always', function() {
 	it('should attach to window.onerror', function() {
 		expect(window.onerror).not.toBe(null);
 	});
 });
 
-describe('When an unhandled error is thrown', function() {
 
+describe('When an unhandled error is thrown', function() {
+	var cSpy;
 	beforeEach(function(done) {
-		spyOn($, "post");
+		cSpy = spyOn(window, "Image").andCallThrough();
 		setTimeout(function() {
 			done();					
-			//throw "unhandled exception";					
+			throw "unhandled exception";					
 		}, 0);
 	});
 
-	it('should make an ajax call', function() {
-		expect($.post).toHaveBeenCalled();
-	});
-});
-
-xdescribe('When foobar', function() {
-
-	beforeEach(function() {
-		spyOn($, "post");
-	});
-
-	it('should make an ajax call', function() {
-		
-		expect(function() { throw new Error(); }).toThrow();
-		expect($.post).toHaveBeenCalled();
+	it('should create an image', function() {
+		expect(cSpy).toHaveBeenCalled();
 	});
 });
